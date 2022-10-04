f85439acb89b6df22e779b27c033720546810527
Вертипрахов Илья Александрович <iavertipra@mts.ru>
2022-10-04 20:08:38 +0500

    initial commit

--- src/app/movies/const.ts
+++ src/app/movies/const.ts
@@ -9,15 +9,15 @@ export const MOCK_MOVIES: Movie[] = [
   },
   {
     id: 2,
-    name: 'Я легенда',
+    name: 'Люди в черном 2',
     poster:
-      'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/60692c49-3ca0-46ed-92d4-db37636f8d5b/300x450',
+      'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/c652b7b1-5c6b-4647-a32b-28a2d15a0eec/300x450',
   },
   {
     id: 3,
-    name: 'Я робот',
+    name: 'Люди в черном 3',
     poster:
-      'https://upload.wikimedia.org/wikipedia/ru/5/50/I_robot_2004_poster1.jpg',
+      'https://upload.wikimedia.org/wikipedia/ru/d/de/MIB3_poster.jpg',
   },
   {
     id: 4,
@@ -25,4 +25,29 @@ export const MOCK_MOVIES: Movie[] = [
     poster:
       'https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/f191a407-17a9-4baf-ac8e-dd2b13612c2a/300x450',
   },
+  {
+    id: 5,
+    name: 'Плохие парни 2',
+    poster:
+      'https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/36a477b2-12a0-4cd4-8a10-0e36df3f2fbb/600x900',
+  },
+  {
+    id: 6,
+    name: 'Я легенда',
+    poster:
+      'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/60692c49-3ca0-46ed-92d4-db37636f8d5b/300x450',
+  },
+  {
+    id: 7,
+    name: 'Я робот',
+    poster:
+      'https://upload.wikimedia.org/wikipedia/ru/5/50/I_robot_2004_poster1.jpg',
+  },
+  {
+    id: 8,
+    name: 'Хэнкок',
+    poster:
+      'https://upload.wikimedia.org/wikipedia/ru/c/c9/Hancockposter.jpg',
+  },
+  
 ];
